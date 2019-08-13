87f69978823f71d1d421460bb7602a7da1c23c08
Fabiano <fabianocatrinck@gmail.com>
2019-08-12 22:36:14 -0300

    temporario

---
+++ comment_test.rb
@@ -0,0 +1,24 @@
+
+PROGRAMAÇÃOGERADORES
+Gerador de Certidões
+Gerador de CNH
+Gerador de Conta Bancária
+Gerador de CPF
+Gerador de Nicks
+Gerador de Nomes
+Gerador de Números Aleatórios
+Gerador de PIS/PASEP
+Gerador de RENAVAM
+Gerador de Veículos
+Gerador Placa de Veículos
+Gerador de CNPJ
+Gerador de CEP
+Gerador de RG
+Gerador Inscrição Estadual
+Gerador Título de Eleitor
+Gerador Cartão de Crédito
+Gerador de Pessoas
+Gerador de Empresas
+Gerador de Imagem
+Gerador de Lorem Ipsum
+Gerador de Senha
---
+++ create_comments.rb
@@ -0,0 +1,6 @@
+<head> 
+<title>Gerador Meta Tag - 4Devs</title> 
+<meta name="author" content="Thiago José de Paula" /> 
+<meta name="keywords" content="gerador meta tag, meta autor, meta keywords" /> 
+<meta name="description" content="Gera as metas tags do seu site contando os caracteres para facilitar sua vida" /> 
+</head>
